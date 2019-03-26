dc48f7bf6b3ef839d827f1c76990aa579efaa4c3
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-26 15:37:48 +0700

    Upgrade to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
 	name: "do-not-disturb",
+	platforms: [
+		.macOS(.v10_10)
+	],
 	targets: [
 		.target(
 			name: "do-not-disturb"
