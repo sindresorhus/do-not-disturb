487ec3ee02831be46be00bc45c06d7c9d1aa127d
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-30 01:17:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const path = require('path');
-const {EventEmitter} = require('events');
+const EventEmitter = require('events');
 const execa = require('execa');
 const electronUtil = require('electron-util/node');
 
